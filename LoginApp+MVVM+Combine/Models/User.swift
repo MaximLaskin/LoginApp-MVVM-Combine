ca133f846847f0f78d20e143932f5c4c6bc128dd
MaximLaskin <maxlaskin@mail.ru>
2023-03-09 21:49:34 +0300

    Initial Commit

---
+++ LoginApp+MVVM+Combine/Models/User.swift
@@ -0,0 +1,11 @@
+//
+//  User.swift
+//  LoginApp+MVVM+Combine
+//
+//  Created by Maxim on 09.03.2023.
+//
+
+struct User {
+    let user: String?
+    let password: String?
+}
---
+++ LoginApp+MVVM+Combine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp+MVVM+Combine
+//
+//  Created by Maxim on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
